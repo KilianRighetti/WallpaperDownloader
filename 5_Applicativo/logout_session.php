5a7c5ab64d5ea613707ce8bdc6420ca28405a012
KilianRighetti <vbj268@student.edu.ti.ch>
2025-12-19 15:35:58 +0100

    Penultima lezione

--- 5_Applicativo/logout_session.php
+++
@@ -1,8 +0,0 @@
-<!-- File che serve a terminare la sessione quando necessario -->
-<?php
-session_start(); // APRE la sessione --> senza quetsa riga, non posso chiuderla
-session_unset(); // Pulisce le variabili della sessione
-session_destroy(); // Distrugge la sessione
-header("Location: index.php"); // Ricarica la Home
-exit;
-?>
